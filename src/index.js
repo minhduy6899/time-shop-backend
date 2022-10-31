1147ad47890f2eea5daf360066f4fcac9580f572
Minh Duy <minhduy140799@gmail.com>
2022-10-31 20:06:33 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom'
+// import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import './index.scss';
@@ -8,14 +9,15 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = document.getElementById('root')
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </Provider>,
+  root
 );
 
 // If you want to start measuring performance in your app, pass a function
